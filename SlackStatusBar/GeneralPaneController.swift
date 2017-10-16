8e45e08c3dd5e6d619320610bc1544659f8fc837
Eric Brown <browne@vmware.com>
2017-10-15 23:44:49 -0700

    Make the preferences toolbar actually work
    Signed-off-by: Eric Brown <browne@vmware.com>

---
+++ SlackStatusBar/GeneralPaneController.swift
@@ -0,0 +1,14 @@
+//
+//  GeneralView.swift
+//  SlackStatusBar
+//
+//  Created by Eric Brown on 7/17/17.
+//  Copyright © 2017 Eric Brown. All rights reserved.
+//
+
+import Cocoa
+
+final class GeneralPaneController: NSViewController {
+    @IBOutlet weak var tokenTextField: NSTextField!
+
+}
